f51c248669683c469111418b3625c57be5847daf
kaumina <1788436+kaumina@users.noreply.github.com>
2023-09-07 12:32:50 -0700

    Init

---
+++ main.tf
@@ -0,0 +1,22 @@
+resource "azurerm_resource_group" "vnet-rg" {
+    name     = vnet-resource-group
+    location = westus
+}
+
+resource "azurerm_virtual_network" "vnet" {
+    name                = "vnet"
+    location            = azurerm_resource_group.vnet-rg.location
+    resource_group_name = azurerm_resource_group.vnet-rg.name
+    address_space       = ["192.168.0.0/16"]
+
+    tags = {
+    environment = demo
+    }
+}
+
+resource "azurerm_subnet" "subnet" {
+    name                 = "SubNet"
+    resource_group_name  = azurerm_resource_group.vnet-rg.name
+    virtual_network_name = azurerm_virtual_network.vnet.name
+    address_prefixes     = ["192.168.255.224/27"]
+}
